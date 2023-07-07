021c157bf6e238dad9dba1cc41f3de673ab9bedf
Muna-Redi <francischym@gmail.com>
2023-07-07 10:48:55 +0100

    typed function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a typed function that returns the sum of
+    two flost numbers given s argument
+"""
+
+def add(a: float, b: float) -> float:
+    """ returns the sum of 2 numbers """
+
+    return a+b
+
