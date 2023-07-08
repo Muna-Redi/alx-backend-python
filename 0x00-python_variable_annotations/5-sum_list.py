fe598d5a632b282fa54cac7526eb3b85cdf99261
Muna-Redi <francischym@gmail.com>
2023-07-08 02:49:21 +0100

    Annotated function with complex type argument

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Annotated function with Complex type argument"""
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    fumction takes lists of floats argument
+    and returns their sum as a float.
+    """
+
+    return sum(input_list)
