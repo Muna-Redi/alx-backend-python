01ccb546f90b13ba18563d81ff193382c5a0281a
Muna-Redi <francischym@gmail.com>
2023-07-08 03:10:27 +0100

    annotated function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Annotsted function """
+from typing import Callable, Iterator, Union, Optional, List, Tuple
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    This function takes a float multiplier as
+    argument, andreturns a function that multiplies
+    a float by multiplier.
+    """
+    def f(n: float) -> float:
+        """ Thid function multiplies a float by a multiplier """
+        return float(n * multiplier)
+
+    return f
